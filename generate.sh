2a3d159859c3fe65f18956fadc5d8c580a8563b8
austin-millan <austin.millan@gmail.com>
2020-05-26 10:40:07 -0700

    fix generator

--- generate.sh
+++ generate.sh
@@ -10,16 +10,16 @@ docker run \
         -i /local/swagger.yml \
         -g go \
         -o /local/openapi
-# rm openapi/api_*.go \
-#     openapi/.travis.yml \
-#     openapi/client.go \
-#     openapi/configuration.go \
-#     openapi/git_push.sh \
-#     openapi/go.mod \
-#     openapi/go.sum \
-#     openapi/response.go \
-#     openapi/api/openapi.yaml \
-#     openapi/docs/*Api.md
+rm openapi/api_*.go \
+    openapi/.travis.yml \
+    openapi/client.go \
+    openapi/configuration.go \
+    openapi/git_push.sh \
+    openapi/go.mod \
+    openapi/go.sum \
+    openapi/response.go \
+    openapi/api/openapi.yaml \
+    openapi/docs/*Api.md
 
 echo "module gitlab.com/trade-hawk/tdameritrade-openapi
 go 1.13" > go.mod
